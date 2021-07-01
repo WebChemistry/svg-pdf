6396b61d51b65715eb7616f077bd39a251002275
MartkCz <martk494@gmail.com>
2021-07-01 09:50:30 +0200

    add polygon()

--- src/Decorator/PdfDecorator.php
+++ src/Decorator/PdfDecorator.php
@@ -17,4 +17,33 @@ final class PdfDecorator extends FPDF
 		return $this->tMargin;
 	}
 
+	/**
+	 * @param array<int|float> $points
+	 */
+	public function polygon(array $points, string $style = 'D'): void
+	{
+		//Draw a polygon
+		if ($style === 'F') {
+			$op = 'f';
+		} elseif ($style === 'FD' || $style === 'DF') {
+			$op = 'b';
+		} else {
+			$op = 's';
+		}
+
+		$h = $this->h;
+		$k = $this->k;
+
+		$points_string = '';
+		for ($i = 0; $i < count($points); $i += 2) {
+			$points_string .= sprintf('%.2F %.2F', $points[$i] * $k, ($h - $points[$i + 1]) * $k);
+			if ($i == 0) {
+				$points_string .= ' m ';
+			} else {
+				$points_string .= ' l ';
+			}
+		}
+		$this->_out($points_string . $op);
+	}
+
 }
